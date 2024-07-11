ed9b7a8fad111bb0326e3837bf8d072548ff99ef
Choxy5 <kristijan.blazevic000@gmail.com>
2024-07-11 11:19:50 +0200

    Home Page Content

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import Logo from '@/components/logo';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header>
+      <Logo />
+      <Link href="/">Home</Link>
+      <Link href="/events/all">All Events</Link>
+    </header>
+  );
+}
