3dbc96df7a6daaf405b0a88afec211ac2dfa4584
Choxy5 <kristijan.blazevic000@gmail.com>
2024-07-11 12:49:36 +0200

    Home Page Styling with TailwindCss

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+
 export default function Logo() {
-  return <h1></h1>;
+  return (
+    <Image
+      src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+      alt="EVENTO logo"
+      width={53}
+      height={12}
+    />
+  );
 }
