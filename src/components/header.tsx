0342a44cb3194380241d6537aacd4cf2c0724b64
Choxy5 <kristijan.blazevic000@gmail.com>
2024-07-15 21:47:15 +0200

    Styling event card page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 'use client';
 import Logo from '@/components/logo';
-import clsx from 'clsx';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 const routes = [
   {
@@ -30,7 +30,7 @@ export default function Header() {
           {routes.map((route) => (
             <li
               key={route.path}
-              className={clsx(
+              className={cn(
                 'hover:text-white flex items-center relative transition',
                 {
                   'text-white': activePathname === route.path,
