2543ddfe0472a82121ba2c29970a14459be8602d
Choxy5 <kristijan.blazevic000@gmail.com>
2024-07-17 00:02:01 +0200

    Data fetching utilities

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,18 +1,29 @@
+import Loading from '@/app/events/[city]/loading';
 import EventsList from '@/components/events-list';
 import H1 from '@/components/h1';
 import { EventoEvent } from '@/lib/types';
-import { sleep } from '@/lib/utils';
+import { capitalize, sleep } from '@/lib/utils';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
 
-type EventsPageProps = {
+type Props = {
   params: {
     city: string;
   };
 };
 
-export default async function EventsPage({ params }: EventsPageProps) {
+export function generateMetadata({ params }: Props): Metadata {
   const city = params.city;
 
-  await sleep(2000)
+  return {
+    title: city === 'all' ? 'All Events' : `Events in ${capitalize(city)}`,
+  };
+}
+
+export default async function EventsPage({ params }: Props) {
+  const city = params.city;
+
+  await sleep(2000);
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
@@ -23,11 +34,12 @@ export default async function EventsPage({ params }: EventsPageProps) {
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
         {city === 'all' && 'All Events'}
-        {city !== 'all' &&
-          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city !== 'all' && `Events in ${capitalize(city)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { EventoEvent } from '@/lib/types';
 import clsx, { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -5,8 +6,30 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export function sleep(ms: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+}
+
+export async function getEvents(city: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+
+  const events: EventoEvent[] = await response.json();
+  return events;
+}
+
+export async function getEvent(slug: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+
+  const event = await response.json();
+  return event;
 }
